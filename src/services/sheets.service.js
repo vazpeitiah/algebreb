f7e4d740417d711ff57a7bf00160a73109732739
VladimirAh <vazpeitiah@gmail.com>
2021-11-03 17:00:43 -0600

    cambiar API_URL

--- src/services/sheets.service.js
+++ src/services/sheets.service.js
@@ -2,11 +2,12 @@ import authService from "./auth.service"
 
 const sheetService = {}
 
-const API_URL = 'http://localhost:5000/sheets'
+//const API_URL = 'http://localhost:5000'
+const API_URL = 'https://algebreb-api.herokuapp.com'
 
 sheetService.getSheetsByUser = async (userId) => {
   try{
-    const res = await fetch(API_URL + '/byuser/' + userId, {
+    const res = await fetch(API_URL + '/sheets/byuser/' + userId, {
       headers: {...authService.authHeader(), "Content-Type": "application/json"}
     })
     const data = await res.json()
@@ -18,7 +19,7 @@ sheetService.getSheetsByUser = async (userId) => {
 
 sheetService.getSheetById = async (sheetId) => {
   try{
-    const res = await fetch(API_URL + '/' + sheetId, {
+    const res = await fetch(API_URL + '/sheets/' + sheetId, {
       headers: {...authService.authHeader(), "Content-Type": "application/json"},
     })
     const data = await res.json()
@@ -30,7 +31,7 @@ sheetService.getSheetById = async (sheetId) => {
 
 sheetService.createSheet = async (params) => {
   try{
-    const res = await fetch(API_URL, {
+    const res = await fetch(API_URL + '/sheets', {
       method: "POST",
       headers: {...authService.authHeader(), "Content-Type": "application/json"},
       body: JSON.stringify(params)
@@ -45,7 +46,7 @@ sheetService.createSheet = async (params) => {
 
 sheetService.updateSheet = async (sheetId, params) => {
   try{
-    const res = await fetch(API_URL + '/' + sheetId, {
+    const res = await fetch(API_URL + '/sheets/' + sheetId, {
       method: "PUT",
       headers: {...authService.authHeader(), "Content-Type": "application/json"},
       body: JSON.stringify(params)
@@ -59,7 +60,7 @@ sheetService.updateSheet = async (sheetId, params) => {
 
 sheetService.deleteSheet = async (sheetId) => {
   try{
-    const res = await fetch(API_URL + '/' + sheetId, {
+    const res = await fetch(API_URL + '/sheets/' + sheetId, {
       method: "DELETE",
       headers: {...authService.authHeader(), "Content-Type": "application/json"}
     })
